89247c2d7dea86615ac0b167b5849ac92e06cc0e
Jordan Frecon <jordan.frecon@gmail.com>
2022-06-14 08:23:12 +0200

    fill models list

--- bregmanet/__init__.py
+++ bregmanet/__init__.py
@@ -1,3 +1,4 @@
 from bregmanet.mlp import MLP
 from bregmanet.resnet_small import bresnet20, bresnet32, bresnet44, bresnet56, bresnet110, bresnet1202
-from bregmanet.resnet_large import bresnet18
+from bregmanet.resnet_large import bresnet18, bresnet34, bresnet50, bresnet101, bresnet152, bresnext50_32x4d,\
+    bresnext101_32x8d, wide_bresnet50_2, wide_bresnet101_2
